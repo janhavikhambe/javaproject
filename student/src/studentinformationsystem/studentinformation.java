0d0ea4a1b4f31c7a4eec4910cda212884a835617
Abhinaya <abhinayaanbu15@gmail.com>
2024-03-21 00:49:09 +0530

    First Commit

---
+++ student/src/studentinformationsystem/studentinformation.java
@@ -0,0 +1,17 @@
+package studentinformationsystem;
+
+public class StudentInformationSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+
+public class studentinformation {
+
+}
